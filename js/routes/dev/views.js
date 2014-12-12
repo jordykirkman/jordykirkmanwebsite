dc6ab0f37ccb7524cb8e0bddb79a8e0ce444a307
Jordan Kirkman <jokirkman@vivint.com>
2014-12-11 19:33:50 -0700

    added basic skill graph

--- js/routes/dev/views.js
+++ js/routes/dev/views.js
@@ -9,18 +9,26 @@ function() {
 
 	App.BarGraph = Ember.CollectionView.extend({
 		classNames: ['graph'],
-		content: [{level: 20}, {level: 20}, {level: 40}, {level: 50}, {level: 50}, {level: 30} ],
-
-		count: function(){
-			return this.get('content').toArray().get('length');
-		}.property('this.content'),
+		content: Ember.A([
+			{level: 80, name: 'Sass, Css3, HTML5'},
+			{level: 90, name: 'Javascript'},
+			{level: 90, name: 'Ember.js'},
+			{level: 60, name: 'PHP'},
+			{level: 40, name: 'Node.js'},
+			{level: 20, name: 'Java'},
+			{level: 10, name: 'Hearthstone'}
+		]),
+		barWidth: function(){
+			var count = this.get('content.length');
+			return (100 - count) / this.get('content.length');
+		}.property('this.count'),
 		itemViewClass: Ember.View.extend({
+			attributeBindings: ['style'],
 			classNames: ['bar'],
-			elementStyle: function(){
-				
-				return 'height: ' + this.get('level') + '%;';
-			}.property('this.level'),
-			// template: Ember.Handlebars.compile('<div {{bind-attr style="view.style"}}></div>')
+			style: function(){
+				return 'height: ' + this.get('content.level') + '%; width: ' + this.get('parentView.barWidth') + '%; top: ' + (100 - this.get('content.level')) + '%;';
+			}.property('this.content.level'),
+			template: Ember.Handlebars.compile('{{view.content.name}}')
 		})
 	});
 
