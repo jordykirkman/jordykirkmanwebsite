862512b848b4287d15e04e687289a2f62566283f
Jordan Kirkman <jokirkman@vivint.com>
2014-12-12 15:33:56 -0700

    finished dynamic skill graph

--- js/routes/dev/views.js
+++ js/routes/dev/views.js
@@ -7,8 +7,28 @@ define(
 	
 function() {
 
+	App.BarGraphContainer = Ember.View.extend({
+		classNames: ['graph-container'],
+		values: Ember.A([
+			{name: 'Godlike'},
+			{name: 'Miltikill'},
+			{name: 'Double Kill'},
+			{name: 'First Blood'},
+			{name: 'Noob'},
+		]),
+		layout: Ember.Handlebars.compile('\
+		<div class="sixteen columns">\
+			<ul>\
+			{{#each view.values}}\
+				<li><h4>{{name}}</h4></li>\
+			{{/each}}\
+			</ul>\
+		</div>\
+		<div class="clear"></div>{{yield}}')
+	});
+
 	App.BarGraph = Ember.CollectionView.extend({
-		classNames: ['graph'],
+		classNames: ['graph', 'thirteen', 'columns'],
 		content: Ember.A([
 			{level: 80, name: 'Sass, Css3, HTML5'},
 			{level: 90, name: 'Javascript'},
@@ -23,7 +43,7 @@ function() {
 			return (100 - count) / this.get('content.length');
 		}.property('this.count'),
 		itemViewClass: Ember.View.extend({
-			attributeBindings: ['style'],
+			attributeBindings: ['style', 'animated'],
 			classNames: ['bar'],
 			style: function(){
 				return 'height: ' + this.get('content.level') + '%; width: ' + this.get('parentView.barWidth') + '%; top: ' + (100 - this.get('content.level')) + '%;';
